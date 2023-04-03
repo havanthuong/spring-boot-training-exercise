e105d40664c24175016711e07019f031091db19b
havanthuong <havanthuong2000@gmail.com>
2023-04-03 17:54:10 +0700

    create the first API to get a user

--- src/main/java/com/thuongha/Main.java
+++ src/main/java/com/thuongha/Main.java
@@ -2,10 +2,20 @@ package com.thuongha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/user")
+    public User createUser() {
+        return new User(10,"HVT");
+    }
+
 }
