dab75502a31c4f0303ee82d11ad976ffa6c0077b
havanthuong <havanthuong2000@gmail.com>
2023-04-04 16:10:05 +0700

    Basic API with users db

--- src/main/java/com/thuongha/Main.java
+++ src/main/java/com/thuongha/Main.java
@@ -2,19 +2,41 @@ package com.thuongha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
+    private final UserRepository userRepository;
+    public Main(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @GetMapping("/user")
-    public User createUser() {
-        return new User(10,"HVT",12,"hgj");
+    @GetMapping("api/v1/users")
+    public List<User> getUsers(){
+        return this.userRepository.findAll();
     }
-
+    @PostMapping("api/v1/user")
+    public ResponseEntity<User> createUser(@Validated @RequestBody User user) {
+        return new ResponseEntity<User>(
+                userRepository.save(user),
+                HttpStatus.CREATED
+        );
+    }
+    @DeleteMapping("api/v1/user/{userId}")
+    public void deleteUse(@PathVariable("userId") Integer id){
+        //find id
+        User user = userRepository.getById(id);
+        if (user == null)
+            //return 404 bad request
+            return;
+        userRepository.deleteById(id);
+    }
 }
--- src/main/java/com/thuongha/User.java
+++ src/main/java/com/thuongha/User.java
@@ -8,6 +8,11 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "user_id_sequence",
+            sequenceName = "user_id_sequence",
+            allocationSize = 1
+    )
     private Integer id;
     private String userName;
     private Integer age;
