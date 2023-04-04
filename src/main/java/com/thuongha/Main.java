fffb049f74b9de3f468b4eac5e9239d17d161010
havanthuong <havanthuong2000@gmail.com>
2023-04-04 09:13:33 +0700

    remove package not use

--- src/main/java/com/thuongha/Main.java
+++ src/main/java/com/thuongha/Main.java
@@ -2,7 +2,6 @@ package com.thuongha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
