1c62ef4c3ba331358e3b6edeeebb64c5703d9a52
havanthuong <havanthuong2000@gmail.com>
2023-04-04 09:39:27 +0700

    correct user model

--- src/main/java/com/thuongha/User.java
+++ src/main/java/com/thuongha/User.java
@@ -1,17 +1,23 @@
 package com.thuongha;
 
+import java.util.Objects;
+
 public class User {
-    private int id;
+    private Integer id;
     private String userName;
+    private Integer age;
+    private String email;
     public User(){}
-    public User(int id, String userName){
-        this.id=id;
-        this.userName=userName;
+    public User(Integer id, String userName, Integer age, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.age = age;
+        this.email = email;
     }
-    public int getId() {
+    public Integer getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
     public String getUserName() {
@@ -19,5 +25,41 @@ public class User {
     }
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(age, user.age) &&
+                Objects.equals(email, user.email);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, age, email);
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
